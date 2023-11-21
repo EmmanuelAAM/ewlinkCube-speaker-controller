8a863515e4b3af624014af3a5bffb1696f2eeb82
EmmanuelAAM <emanuel.acuna.mora@gmail.com>
2023-11-21 10:29:12 -0600

    Update publish.sh

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# 1. Input Docker image name
-read -p "Docker image name: " $0
-
-# 2. Input Docker image version
-read -p "Docker image version: " image_version
-
 # 3. Build Docker image
 docker build -t $0 --platform=linux/arm/v7 .
 docker build -t $0:v$1 --platform=linux/arm/v7 .
