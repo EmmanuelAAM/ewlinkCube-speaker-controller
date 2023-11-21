4c388ed1cfb0ae0f695f21f5f7f3e24c342b199f
EmmanuelAAM <emanuel.acuna.mora@gmail.com>
2023-11-21 17:07:48 -0600

    Update publish.sh

--- docker/publish.sh
+++ docker/publish.sh
@@ -8,7 +8,6 @@ docker login -u=$usr -p=$psw
 # 3. Push Docker image
 docker push $name
 docker tag $name $usr/$name:v$version
-docker push $usr/$name:v$version
-
+docker push emmanuelam93/ewlink-cube-speaker-control:v$version
 # 4. Logout Docker Hub account
 docker logout
