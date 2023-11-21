7cadd5b0eb47552ac84e6931ade2578712f70ece
EmmanuelAAM <emanuel.acuna.mora@gmail.com>
2023-11-21 17:42:08 -0600

    Update publish.sh

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # 1. Build Docker image
-docker buildx build --platform=linux/arm/v7 -t ewlink-cube-speaker-control:latest  .
-wait
+#docker buildx build --platform=linux/arm/v7 -t ewlink-cube-speaker-control:latest  .
 # 2. Login Docker Hub account
 docker login -u=$usr -p=$psw
-
+docker buildx build --platform=linux/arm/v7 -t ewlink-cube-speaker-control:latest  .
 # 3. Push Docker image
 docker tag ewlink-cube-speaker-control emmanuelam93/ewlink-cube-speaker-control:latest
 docker push emmanuelam93/ewlink-cube-speaker-control:latest
