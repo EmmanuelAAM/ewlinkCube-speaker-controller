946ebe4d08043e1127006b24b251688eed8e7b01
EmmanuelAAM <emanuel.acuna.mora@gmail.com>
2023-11-21 16:28:30 -0600

    Update publish.sh

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 1. Build Docker image
-docker build -t $name:v$version --platform=linux/arm/v7 .
+docker buildx build -t $name:v$version --platform=linux/arm/v7 .
 
 # 2. Login Docker Hub account
 docker login -u=$usr -p=$psw
