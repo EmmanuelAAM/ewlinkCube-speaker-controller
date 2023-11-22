e846511ea53497c9c1e67c56c6acef34ef858202
EmmanuelAAM <emanuel.acuna.mora@gmail.com>
2023-11-21 20:19:34 -0600

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,18 +5,18 @@ rm -rf build && mkdir build
 npx lerna run build
 
 
-cp -r ../packages/server/dist build/server
+cp -r ../packages/server/dist ../build/server
 
-cp -r ../packages/web/dist build/public
+cp -r ../packages/web/dist ../build/public
 
 ls build
 
 ls build/server
 ls build/public
 
-cp ../docker/Dockerfile build
-cp ../docker/publish.sh build
-cp ../docker/.dockerignore build
+cp ../docker/Dockerfile ../build
+cp ../docker/publish.sh ../build
+cp ../docker/.dockerignore ../build
 
 cat << EOF > ./build/buildinfo
 Build Version: $version
